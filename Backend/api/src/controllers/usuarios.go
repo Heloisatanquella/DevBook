b93cf312e4b7597902d2896a4fb87ef00302690f
heloisatanquella <heloisacristovao02@gmail.com>
2024-12-23 09:57:10 -0300

    feat: struct of controllers

---
+++ Backend/api/src/controllers/usuarios.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarUsuario insere um usuario no banco de dados
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Criando usuário!"))
+}
+
+// BuscarUsuarios busca os usuarios do banco de dados
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando todos os usuários!"))
+}
+
+// BuscarUsuarioUnico busca um usuario no banco de dados
+func BuscarUsuarioUnico(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando um usuário!"))
+}
+
+// AtualizarUsuario atualiza um usuario no banco de dados
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Atualizando o usuário!"))
+}
+
+// DeletarUsuario exclui um usuario do banco de dados
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Excluindo o usuário!"))
+}
