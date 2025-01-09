88c08b2c0152a670d25df25775369dd5e740bf20
heloisatanquella <heloisacristovao02@gmail.com>
2025-01-09 11:19:35 -0300

    feat: email validation

--- Backend/api/src/entities/usuario.go
+++ Backend/api/src/entities/usuario.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 // Usuario representa um usuario utilizando a rede social
@@ -35,6 +37,11 @@ func (usuario *Usuario) validar(etapa string) error {
 	if usuario.Email == "" {
 		return errors.New("o e-mail é obrigatório e não pode estar em branco")
 	}
+
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("o e-mail inserido é inválido")
+	}
+
 	if etapa == "cadastro" && usuario.Senha == "" {
 		return errors.New("a senha é obrigatória e não pode estar em branco")
 	}
