059244f82904871f57de40884191960849f000be
heloisatanquella <heloisacristovao02@gmail.com>
2025-01-24 10:29:24 -0300

    feat: BuscarPorID method on publications

--- Backend/api/src/repositorios/publicacoes.go
+++ Backend/api/src/repositorios/publicacoes.go
@@ -15,7 +15,7 @@ func NovoRepositorioDePublicacoes(db *sql.DB) *Publicacoes {
 	return &Publicacoes{db}
 }
 
-// Criar insete uma publicacao no banco de dados
+// Criar insere uma publicacao no banco de dados
 func (repositorio Publicacoes) Criar(publicacao entities.Publicacao) (uint64, error) {
 	statement, erro := repositorio.db.Prepare(
 		"insert into publicacoes (titulo, conteudo, autor_id) values (?, ?, ?)",
@@ -37,4 +37,35 @@ func (repositorio Publicacoes) Criar(publicacao entities.Publicacao) (uint64, er
 
 	return uint64(ultimoIDInserido), nil
 
+}
+
+// BuscarPorID traz uma única publicacao do benco de dados
+func (repositorio Publicacoes) BuscarPorID(publicacaoID uint64) (entities.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+		select p.*, u.nick from
+		publicacoes p inner join usuarios u
+		on u.id = p.autor_id where p.id = ?`, publicacaoID,
+	)
+	if erro != nil {
+		return entities.Publicacao{}, erro
+	}
+	defer linhas.Close()
+
+	var publicacao entities.Publicacao
+
+	if linhas.Next() {
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return entities.Publicacao{}, erro
+		}
+	}
+
+	return publicacao, nil
 }
