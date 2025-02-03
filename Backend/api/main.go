e2241faeb42c1d897ce00a9b2d220b62daba0405
heloisatanquella <heloisacristovao02@gmail.com>
2025-02-03 10:23:50 -0300

    fix: add a function to enable cors

--- Backend/api/main.go
+++ Backend/api/main.go
@@ -8,11 +8,29 @@ import (
 	"net/http"
 )
 
+// Middleware para habilitar CORS
+func habilitarCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:6001")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	config.Carregar()
 
 	r := router.Gerar()
 
+	handler := habilitarCORS(r)
+
 	fmt.Printf("Escutando na porta %d", config.Porta)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), handler))
 }
