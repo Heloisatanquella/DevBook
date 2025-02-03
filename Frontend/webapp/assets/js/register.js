f698a1acbb0a42680707fc12de2c8ee9e8a56e84
heloisatanquella <heloisacristovao02@gmail.com>
2025-02-03 10:26:22 -0300

    feat: error and success handling in ajax

--- Frontend/webapp/assets/js/register.js
+++ Frontend/webapp/assets/js/register.js
@@ -1,22 +1,26 @@
-// $(document).ready(function () {
-// });
+
 $('#register-form').on('submit', criarUsuario);
 
 function criarUsuario(evento) {
     evento.preventDefault();
 
     if ($('#senha').val() != $('#confirmar-senha').val()) {
-        Swal.fire("Ops...", "As senhas não coincidem!", "error");
+        alert("Ops...as senhas não coincidem!");
         return;
     }
     $.ajax({
-        url: "/usuarios",
+        url: "http://localhost:5000/usuarios",
         method: "POST",
-        data: {
-           nome: $('#nome').val(), 
-           email: $('#email').val(),
-           nick: $('#nick').val(),
-           senha: $('#senha').val()
-        }
-    })
+        contentType: "application/json",
+        data: JSON.stringify({
+            nome: $('#nome').val(), 
+            email: $('#email').val(),
+            nick: $('#nick').val(),
+            senha: $('#senha').val()
+        })
+    }).done(function() {
+        alert("Usuário cadastrado com sucesso!");
+    }).fail(function() {
+        alert("Erro ao cadastrar o usuário!");
+    });
 }
