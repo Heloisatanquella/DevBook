0e7c53446282d66af85d326d054e71894d6e8e78
heloisatanquella <heloisacristovao02@gmail.com>
2025-02-18 15:36:23 -0300

    feat: render posts on home page

--- Frontend/webapp/src/controllers/paginas.go
+++ Frontend/webapp/src/controllers/paginas.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/entities"
 	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -22,8 +25,23 @@ func CarregarTelaCadastro(w http.ResponseWriter, r *http.Request) {
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
 	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
-	fmt.Println(response.StatusCode, erro)
-	fmt.Println(url)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
 
-	utils.ExecutarTemplate(w, "home.html", nil)
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	var publicacoes []entities.Publicacao
+	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	fmt.Println(publicacoes)
+	utils.ExecutarTemplate(w, "home.html", publicacoes)
 }
