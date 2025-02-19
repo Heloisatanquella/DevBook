961f39c93a755c47feeb1ee282808ec1b9e45147
heloisatanquella <heloisacristovao02@gmail.com>
2025-02-19 14:51:05 -0300

    feat: identifying the logged in user

--- Frontend/webapp/src/controllers/paginas.go
+++ Frontend/webapp/src/controllers/paginas.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/src/config"
+	"webapp/src/cookies"
 	"webapp/src/entities"
 	"webapp/src/requisicoes"
 	"webapp/src/respostas"
@@ -42,6 +44,14 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(publicacoes)
-	utils.ExecutarTemplate(w, "home.html", publicacoes)
+	cookie, _ := cookies.Ler(r)
+	usuarioID, _ := strconv.ParseUint(cookie["id"], 10, 64)
+
+	utils.ExecutarTemplate(w, "home.html", struct {
+		Publicacoes []entities.Publicacao
+		UsuarioID   uint64
+	}{
+		Publicacoes: publicacoes,
+		UsuarioID:   usuarioID,
+	})
 }
