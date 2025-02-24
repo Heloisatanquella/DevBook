6affb32cf3a0239eb6b2c14c5276f4e6318f11a2
heloisatanquella <heloisacristovao02@gmail.com>
2025-02-24 15:13:35 -0300

    feat: API call to delete a post

--- Frontend/webapp/src/controllers/publicacoes.go
+++ Frontend/webapp/src/controllers/publicacoes.go
@@ -108,19 +108,46 @@ func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	r.ParseForm()
-	publicacao, erro := json.Marshal(map[string]string{
-		"titulo":   r.FormValue("titulo"),
-		"conteudo": r.FormValue("conteudo"),
-	})
+	var publicacao map[string]string
 
+	if err := json.NewDecoder(r.Body).Decode(&publicacao); err != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: "Erro ao ler JSON da requisição"})
+		return
+	}
+
+	publicacaoJSON, erro := json.Marshal(publicacao)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: "Erro ao converter JSON"})
+		return
+	}
+
+	url := fmt.Sprintf("%s/publicacoes/%d", config.APIURL, publicacaoID)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodPut, url, bytes.NewBuffer(publicacaoJSON))
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, nil)
+}
+
+// DeletarPublicacao chama a API para deletar uma publicacao
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
 	if erro != nil {
 		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
 		return
 	}
 
 	url := fmt.Sprintf("%s/publicacoes/%d", config.APIURL, publicacaoID)
-	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodPut, url, bytes.NewBuffer(publicacao))
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodDelete, url, nil)
 	if erro != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
 		return
