62617e0cf2617bee380245a0626dacd195a85eb2
heloisatanquella <heloisacristovao02@gmail.com>
2025-02-25 14:25:07 -0300

    feat: unfollow user functionality

--- Frontend/webapp/src/controllers/usuarios.go
+++ Frontend/webapp/src/controllers/usuarios.go
@@ -5,10 +5,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/src/config"
+	"webapp/src/requisicoes"
 	"webapp/src/respostas"
+
+	"github.com/gorilla/mux"
 )
 
+// CriarUsuario chama a API para criar um usuario no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	var usuario map[string]string
 
@@ -42,4 +47,37 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respostas.JSON(w, response.StatusCode, nil)
+}
+
+// ParaDeSeguirUsuario chama a API para parar de seguir um usuario
+func ParaDeSeguirUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s/usuarios/%d/parar-de-seguir", config.APIURL, usuarioID)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodPost, url, nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	if response.StatusCode != http.StatusNoContent {
+		defer response.Body.Close()
+	}
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	if response.StatusCode == http.StatusNoContent {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	respostas.JSON(w, response.StatusCode, nil)
+
 }
